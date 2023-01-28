ecd72e230cd6cd7bc5bb9416f5a79cc1a3bce70f
yongj.zhuang <yongj.zhuang@outlook.com>
2023-01-28 17:16:54 +0800

    Load cache on startup

--- goauth/main.go
+++ goauth/main.go
@@ -24,6 +24,20 @@ func main() {
 		}
 	})
 
+	// for the first time
+	server.OnServerBootstrapped(func() {
+		ec := common.EmptyExecContext()
+		if e := domain.LoadRoleResCache(ec); e != nil {
+			ec.Log.Errorf("Failed to load role resource, %v", e)
+		}
+	})
+	server.OnServerBootstrapped(func() {
+		ec := common.EmptyExecContext()
+		if e := domain.LoadPathResCache(ec); e != nil {
+			ec.Log.Errorf("Failed to load path resource, %v", e)
+		}
+	})
+
 	// open-api endpoints
 	server.PostJ(server.OpenApiPath("/resource/add"),
 		func(c *gin.Context, ec common.ExecContext, req domain.CreateResReq) (any, error) {
